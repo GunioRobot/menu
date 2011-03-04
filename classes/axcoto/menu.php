2af2c59cf111c3a42d7fb9a7e40a8e7a91c4a9a3
Vincent Nguyen <kureikain@gmail.com>
2011-03-03 23:22:51 -0600

    Detect current menu on sub page and index page (default action)

--- classes/axcoto/menu.php
+++ classes/axcoto/menu.php
@@ -48,13 +48,29 @@ abstract class Axcoto_Menu {
         return self::$_instance[$name];
     }
 
+    /**
+     *  Render a menu! We must calculate current menu item at this point because after creating a menu, other menu items can be added to!
+     * @param <type> $attr attribute of menu(class, id)
+     * @return <type> html text of menu
+     */
     public function render($attr=array()) {
         $_attr = array(
             'class' => '',
             'id' => 'menu_' . $this->_name
         );
+        
         $_attr = Arr::overwrite($_attr, $attr);
-        $currentUri = Request::current()->uri();
+
+        $currentUri = Request::current()->uri() . '/';
+        $currentUri = Text::reduce_slashes($currentUri);
+        foreach ($this->_menu as $key=>$item) {
+            $menuUri = Text::reduce_slashes($item['uri'] . '/');
+            $menuDefaultUri = Text::reduce_slashes($item['uri'] . '/index/');
+            if ($menuDefaultUri==$currentUri || $menuUri==$currentUri || strpos($currentUri, $menuUri)===0) {
+                $this->_menu[$key]['current'] = true;
+            }
+        }
+
         $template = View::factory('menu/menu')
                         ->set('menu', $this->_menu)
                         ->set('attr', $_attr)
